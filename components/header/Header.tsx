1561e5ba38824e47e8bdc81a5bb1c6e9064e7825
Kiyarsh <kiyarashhadidian@gmail.com>
2022-11-20 07:41:29 +0330

    Add footer and header

---
+++ components/header/Header.tsx
@@ -0,0 +1,14 @@
+import Grid2 from "@mui/material/Unstable_Grid2/Grid2";
+import { Container, Typography } from "@mui/material";
+
+
+export default function Header() {
+    return(
+        <Grid2 className="my-14">
+            <Container className="text-center">
+                <Typography variant="h4" className="font-bold mb-4">آقای رهگذر</Typography>
+                <Typography variant="body2" className="text-gray-600">آقای رهگذر نویسنده ای خیالی است که متنی موقت برای طراحان گرافیک و وبسایت مینویسد</Typography>
+            </Container>
+        </Grid2>
+    );
+}
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,7 +1,21 @@
 import '../styles/globals.css'
 import type { AppProps } from 'next/app'
 import 'tailwindcss/tailwind.css';
+import "@fortawesome/fontawesome-svg-core/styles.css";
+import { createTheme, ThemeProvider } from "@mui/material/styles";
+// import { config } from "@fontawesome/fontawesome-svg-core";
+// config.autoAddCss = false;
+
+export let theme = createTheme({
+  typography: {
+    fontFamily: "Vazirmatn, sans-serif",
+  }
+});
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <ThemeProvider theme={theme}>
+      <Component {...pageProps} />
+    </ThemeProvider>
+  );
 }

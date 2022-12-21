be79a51a099034bf20927790adf9cd7c0ed3b218
alixo <fromiran98@gmail.com>
2022-12-21 15:36:25 +0330

    Update team images

--- pages/ourTeam.tsx
+++ pages/ourTeam.tsx
@@ -40,13 +40,13 @@ export default function Ourteam() {
             رهگذر نویسنده ای خیالی است که متنی موقت برای طراحان گرافیک و وبسایت مینویسد
           </Typography>
           </Grid2>
-        <Grid2 className="flex justify-between flex-col sm:flex-row sm:flex-wrap md:my-[3.90rem] px-7">
+        <Grid2 className="flex justify-between flex-col sm:flex-row-reverse sm:flex-wrap md:my-[3.90rem] px-7">
           {
             member.map((val, index) => {
               return(
                 <Grid2 key={index} className=" my-5 md:my-0 w-10/12 m-auto sm:w-5/12 md:w-[21%] md:m-0 shadow-md md:shadow-md rounded-lg px-6 md:px-2 py-4">
                   <Grid2 className="flex justify-center">
-                    <Image src={val.picture} alt="Team Member" className="w-40 h-32 md:w-[8.75rem] md:h-28 rounded-md object-cover" />
+                    <Image src={val.picture} alt="Team Member" className="w-32 h-32 md:w-[8.75rem] md:h-[8.75rem] rounded-md object-cover" />
                   </Grid2>
                   <Typography variant="subtitle2" className="text-black text-center sm:text-right  mt-2">{val.name}</Typography>
                   <Typography className="text-gray-500 text-center sm:text-right text-xs mt-1">{val.job}</Typography>
